31ac0b9ef1825527cf314c34601ced186a57e79a
Samandar <samandarabdullaev393@gmail.com>
2022-08-04 20:05:39 +0500

    no-finished code

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
-import { Analytics, News, Settings } from '../icon/index.js';
+import { Analytics, Dashboard, Inbox, Logo, News, Order, Schedules, Settings } from '../icon/index.js';
 import '../../assets/styles/base.scss'
 class Sidebar extends Component {
 
     state = {}
     render() {
         return (
-            <div className="sidebar">
-                <h1>Samandar</h1>
-                <div className="settings">
-                    {Settings}
+            <>
+                <div className="container">
+                    <div className="sidebar">
+                        <div className="logo">
+                            {Logo}
+                        </div>
+                        <div className="search">
+                            <input type="text" />
+                        </div>
+                        <div className="nav-list">
+                            <div className="nav-item">
+                                <ul>
+                                    <li><a href="#">{Dashboard} Dashboard</a></li>
+                                    <li><a href="#">{Order} Orders</a></li>
+                                    <li><a href="#">{Schedules} Messages</a></li>
+                                    <li><a href="#">{Inbox} Inbox</a></li>
+                                    <li><a href="#">{Analytics} Analytics</a></li>
+                                    <li><a href="#">{News} News</a></li>
+                                    <li><a href="#">{Settings} Settings</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="footer">
+                            <div className="img-box">
+
+                            </div>
+                            <div className="description">
+                                <h2>Richard</h2>
+                                <p>9394lay@gmail.com</p>
+                            </div>
+                            <div className="logOut">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="content">
+
+                    </div>
                 </div>
-                <div className="news">
-                    {News}
-                </div>
-                {Analytics}
-            </div>
+
+            </>
         );
     }
 }
